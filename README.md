b579159ec6b7f27b8c804bb17e2d4fd0a5026b8e
Ahmed Muyen Kamran <akamran@colgate.edu>
2022-04-29 14:32:22 +0100

    complete

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 ## Lab feedback
 
  * How long did you spend on this lab?
+	 * A few hours
 
  * What did you think about it?  What was good?  What could be improved?
+	 * I thought it was good for understanding how models relate to each other and also using user authentication
 
 ## Feedback
 
--- products/models.py
+++ products/models.py
@@ -7,9 +7,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Product(models.Model):
     name = models.CharField(max_length=50, blank=False)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, validators=[MinValueValidator(0)])
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, validators=[MinValueValidator(0.0)])
     minimum_age_appropriate = models.IntegerField(default=0, blank=False, validators=[MinValueValidator(0)])
-    maximum_age_appropriate = models.IntegerField(default=-1, blank=False, validators=[MinValueValidator(minimum_age_appropriate)])
+    maximum_age_appropriate = models.IntegerField(default=-1, blank=False, validators=[MinValueValidator(minimum_age_appropriate), MaxValueValidator(135)])
     release_date = models.DateField()
 
     def __str__(self):
